dd25070c7388a1bb2dc6d4cd4a75fba7ca6a2945
zhaozixing <zhaozixing@jd.com>
2018-12-05 01:09:25 +0800

    #add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ArticleComponent } from './article/article.component';
+import { MenuComponent } from './menu/menu.component';
+import { TagComponent } from './tag/tag.component';
+import { ForumComponent } from './forum/forum.component';
+import { FriendComponent } from './friend/friend.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ArticleComponent,
+    MenuComponent,
+    TagComponent,
+    ForumComponent,
+    FriendComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
